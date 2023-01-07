b29313a44b9c6c75b8cc2915b5dd37f807404493
Welness-D <122007592+Welness-D@users.noreply.github.com>
2023-01-07 15:28:42 +0800

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,9 @@
+
+range(start, stop[, step])
+
+map(function, iterables)  ==>  return Iterator    将[1]的每个元素按[0]操作
+
+reduce(function, iterable[, initializer]) ==>  return 函数计算结果   [1]中的第1、2个按[0]操作,再继续向后一直按[0]操作
+                           可选，初始参数
+  
+filter(function, iterable)  ==>  return Iterator   [1]中每个元素按[0]判断，将返回值为TRUE的元素放到Iterator中
