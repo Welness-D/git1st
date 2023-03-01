2a21714fb1fbbe4026c00abb901fb0ac44295568
Welness-D <122007592+Welness-D@users.noreply.github.com>
2023-03-01 16:15:43 +0800

    Add files via upload

--- PTA/L1-064.py
+++ PTA/L1-064.py
@@ -1,23 +1,30 @@
+import re
 N = int(input())
-prompt = ["duo chi yu!", "duo chi rou!",
-          "wan mei!", "ni li hai!", "shao chi rou!"]
-standard = [[129, 25], [130, 27]]
+
 arr = []
 
 for _ in range(N):
-    arr.append(list(map(int, input().split())))
-for i in arr:
-    a = i[0]
-    if i[1] > standard[a][0]:
-        print(prompt[3],end=" ")
-    elif i[1] < standard[a][0]:
-        print(prompt[0],end=" ")
-    else:
-        print(prompt[2],end=" ")
-        
-    if i[2] > standard[a][1]:
-        print(prompt[4])
-    elif i[2] < standard[a][1]:
-        print(prompt[1])
-    else:
-        print(prompt[2])
+    arr.append(input())
+pattern = re.compile(r'\b(can|could) you\b')
+pattern2 = re.compile(r'\bI\b')
+pattern3 = re.compile(r'(\bI\b)|(\b(can|could) you\b)')
+
+for v in arr:
+    print(v)
+    # 处理空格
+    v = re.sub(r"\s+", ' ', v)
+    v = re.sub(r"(^[\s])|([\s]$)", '', v)
+    v = re.sub(r"\s+(?=[^A-Za-z0-9\s+])", '', v)
+    # 大写变小写
+    v = re.sub(r"[A-HJ-Z]", lambda x: x.group().lower(), v)
+    # I me you ,could you ,can you 转换
+    # 替换I和me
+    v = re.sub(r'\b(I|me)\b', r'yyoouu', v)    
+    # 替换can you和could you
+    v = re.sub(r'\b(can|could) you\b', r'I \1', v)
+     # 替换I和me
+    v = re.sub(r'(yyoouu)+', r'you', v)    
+    # 处理问号
+    v = re.sub(r"\?", '!', v)
+
+    print("AI: "+v)
